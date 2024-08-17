8c99a3b43fb7a9b1745c413ca0e2ddca69e3c90f
Artem_PC <taftoos2@gmail.com>
2024-08-17 14:03:04 +0300

    feat: digit morse decoder

--- src/index.js
+++ src/index.js
@@ -1,44 +1,73 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrayData = [];
+    let result = '';
+    // нарезка на 10-кратные куски
+    for (let i = 0, j = 0; i < expr.length; i += 10, j++) {
+        arrayData[j] = expr.substring(i, i + 10);
+    }
+    // перевод из цифр в символы морзе
+    for (let i = 0; i < arrayData.length; i++) {
+        const translated = digitsToMorse(arrayData[i]);
+        result += translated;
+    }
+    return result;
+}
+
+// формирование символа
+function digitsToMorse(input) {
+    let translated = '';
+    if (input == '**********') {
+        return ' ';
+    }
+
+    for (let i = input.length; i >= 2; i -= 2) {
+        const code = input.substr(i - 2, 2);
+
+        switch (code) {
+            case '10': translated = '.' + translated; break;
+            case '11': translated = '-' + translated; break;
+        }
+    }
+    return MORSE_TABLE[translated];
 }
 
 module.exports = {
